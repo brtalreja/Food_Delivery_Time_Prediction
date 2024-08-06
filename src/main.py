a4bab645ddfe6d3a5ade35e2891058779e6cab4c
brtalreja <110854777+brtalreja@users.noreply.github.com>
2024-08-05 23:48:39 -0500

    Preliminary data checks

--- src/main.py
+++ src/main.py
@@ -7,3 +7,10 @@ import plotly.express as px
 #Loading the data
 data = pd.read_csv("../data/deliverytime.txt")
 print(data.head())
+
+#Getting to know the data
+data.info()
+
+print(data.isnull().sum())
+
+#As we do not have any null values, we can move further in our task.
