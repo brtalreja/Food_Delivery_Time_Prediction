755ba99593a4d71d437cd0a9c855ee6daaf3e388
brtalreja <110854777+brtalreja@users.noreply.github.com>
2024-08-05 23:41:53 -0500

    Step1: Libraries import and data loading

---
+++ src/main.py
@@ -0,0 +1,9 @@
+#Importing required libraries
+
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+#Loading the data
+data = pd.read_csv("../data/deliverytime.txt")
+print(data.head())
