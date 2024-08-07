e3766d26082fa480e28807a9b8c23d9d17f4e461
brtalreja <110854777+brtalreja@users.noreply.github.com>
2024-08-07 00:12:02 -0500

    Step 3: EDA continued

--- src/main.py
+++ src/main.py
@@ -67,12 +67,41 @@ figure.write_image("../output/relationship_between_distance_and_delivery_time.pn
 
 # COMMENTS: 
 # Observations:
-# From the scatter plot, we can see two distinct clusters, i.e., first one around 5k distance and second around 20k distance.
+# From the scatter plot, we can see three distinct clusters, i.e., first one around 0k distance, second around 5k distance and final around 20k distance.
+# For distance around 0k, the time taken varies a lot, i.e. from 10 minutes to 50 minutes, in this cluster, the distance varies from 1 unit to 20 units.
 # For distances around 5k, the time taken shows a variation between 10 minutes and 50 minutes.
 # For distances around 20k, the time taken is again varied but appears to be slightly concentrated between 20 minutes to 40 minutes.
 # The blue trend line seems horizontal which indicates that there is no strong correlation between distance and time taken in this data set.
 # 
 # Insights:
-# As the horizontal trend line tells that the time taken does not increase linearly with the distance. The reasons for this could be due to various factors such as traffic conditions, varying speed limits, or differences in the time required for short vs. long distances.
+# beta1 = -2.14e-5, beta0 = 26.67, R_squared = 0.000006. As the horizontal trend line tells that the time taken does not increase linearly with the distance. The reasons for this could be due to various factors such as traffic conditions, varying speed limits, or differences in the time required for short vs. long distances.
 # The distinct clusters might indicate that the distances are not continuous but rather fall into specific categories (e.g., different zones for delivery, specific routes, etc.). 
 # The wide range of time taken for each distance suggests high variability, possibly due to external factors not captured in the data, such as waiting times, delivery handling times, or service efficiency.
+
+#Relationship between the delivery time and age of the driver.
+
+figure = px.scatter(data_frame = data,
+                    x = "Delivery_person_Age",
+                    y = "Time_taken(min)",
+                    size = "Time_taken(min)",
+                    color = "distance",
+                    trendline = "ols",
+                    title = "Relationship between Time taken (mins) and Driver's Age",
+                    labels = {"Delivery_person_Age": "Delivery person's age", "Time_taken(min)": "Time Taken (mins)", "distance": "Distance"})
+
+figure.show()
+
+figure.write_image("../output/relationship_between_driver_age_and_time_taken.png")
+
+# COMMENTS: 
+# Observations:
+# From the scatter plot, we can see various distinct clusters as per the age values, majority of the clusters can be seen from age = 20 to age = 39, the two extremes are age = 15 and age = 50.
+# For most of the age ranges, we can see a variation between 10 minutes to 54 minutes for 3 to 20 units of distance.
+# Drivers belonging to age of 15, 29, and 50 were seen to go for longer delivery routes. Mostly age = 50 were given tasks to deliver to large distances.
+# The trend line shows a linear relationship between driver's age and the time taken for delivery. This indicates that young delivery partners take less time to deliver the food compared to the elder partners.
+# 
+# Insights:
+# beta1 = 0.48, beta0 = 12.05, R_squared = 0.086. As the age of the driver increases, the time taken for delivery also tends to increase. Specifically, for each additional year of the driver's age, the delivery time increases by approximately 0.48 minutes on average.
+# Drivers in the 15-29 age group generally have lower delivery times. The variability in delivery time within this group could be due to factors such as experience, familiarity with routes, and possibly higher physical stamina.
+# Drivers in the 30-39 age group shows a wider range of delivery times, which could be attributed to a mix of factors including type of delivery vehicle, varying levels of experience, differences in driving habits, and potentially more cautious driving behavior compared to younger drivers.
+# Drivers in their 50s, tend to have longer delivery times. This could be due to more cautious driving, possible physical limitations, or preference for routes that might be less congested but longer in distance.
